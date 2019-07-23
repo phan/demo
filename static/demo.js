e360d3d9963528e74bcdc5c0f4703f0bf27fb002
Tyson Andre <tysonandre775@hotmail.com>
2019-07-23 15:55:20 -0400

    Properly clear the UI

--- static/demo.js
+++ static/demo.js
@@ -21,60 +21,94 @@ var output_area = document.getElementById('output');
 
 var phpModule;
 var combinedOutput = '';
+var combinedHTMLOutput = '';
 
-function doRun(code, outputIsHTML) {
+function getOrDefault(value, defaultValue) {
+    return value !== '' ? value : defaultValue;
+}
+
+function doRun(code, outputIsHTML, defaultText) {
+    output_area.innerHTML = '';
     code = code + "\necho PHP_EOL;" // flush line buffer
     console.log('evaluating code', code);
-    combinedOutput = '';
-    let ret = phpModule.ccall('pib_eval', 'number', ["string"], [code])
-    console.log('done evaluating code', ret);
-    if (ret != 0) {
-        combinedOutput += "Error, please check your code";
-    }
-    if (outputIsHTML && ret == 0) {
-        output_area.innerHTML = combinedOutput.replace(/\n/g, "");
-    } else {
-        output_area.innerText = combinedOutput;
-    }
+    let invokePHP = function () {
+        combinedOutput = '';
+        combinedHTMLOutput = '';
+        let ret = phpModule.ccall('pib_eval', 'number', ["string"], [code])
+        console.log('done evaluating code', ret);
+        if (ret != 0) {
+            combinedOutput += "Error, please check your code";
+            combinedHTMLOutput += "Error, please check your code";
+        }
+        if (outputIsHTML && ret == 0) {
+            output_area.innerHTML = getOrDefault(combinedHTMLOutput.replace(/\n/g, ""), defaultText);
+        } else {
+            output_area.innerText = getOrDefault(combinedOutput, defaultText);
+        }
+        enableButtons();
+    };
+    // This works around an issue seen in firefox where
+    // the browser's appearance won't update because JS(emscripten) is still running.
+    // This tries to ensure that buttons get hidden properly and output gets cleared.
+    requestAnimationFrame(function () {
+        setTimeout(invokePHP, 0);
+    });
 }
-function doRunWithWrapper(analysisWrapper, code, outputIsHTML) {
+function doRunWithWrapper(analysisWrapper, code, outputIsHTML, defaultText) {
     // single quotes aren't escaped by encodeURIComponent, but double quotes are.
     // Other problematic characters are escaped, and this preserves UTF-8.
     var contentsFragment = 'rawurldecode("' + encodeURIComponent(code) + '")';
     var analysisCode = analysisWrapper.replace('$CONTENTS_TO_ANALYZE', contentsFragment);
     console.log(analysisCode);
 
-    doRun(analysisCode, outputIsHTML);
+    doRun(analysisCode, outputIsHTML, defaultText);
 }
 
 var didInit = false;
+
+var buttons = [run_button, analyze_button];
+
+function enableButtons() {
+    run_button.textContent = "Run"
+    analyze_button.textContent = "Analyze"
+    for (var button of buttons) {
+        button.disabled = false
+        button.classList.remove('disabled')
+    }
+}
+
+function disableButtons() {
+    for (var button of buttons) {
+        button.disabled = true
+        button.classList.add('disabled')
+    }
+}
 
 function init() {
     didInit = true;
     // This is a monospace element without HTML.
     output_area.innerText = "Click ANALYZE";
-
-    run_button.textContent = "Run"
-    analyze_button.textContent = "Analyze"
-    run_button.disabled = false
-    run_button.classList.remove('disabled')
-    analyze_button.disabled = false
-    analyze_button.classList.remove('disabled')
+    enableButtons();
 
     run_button.addEventListener('click', function () {
         output_area.innerText = '';
+        run_button.textContent = "Running"
+        disableButtons();
         var code = editor.getValue();
         var query = new URLSearchParams();
         if (code.length < 1024) {
             query.append('code', encodeURIComponent(code));
             history.replaceState({}, document.title, "?" + query.toString());
         }
-        var analysisWrapper = 'try { error_reporting(E_ALL); ini_set("display_errors", "stderr"); echo "Set display_errors\n"; eval($CONTENTS_TO_ANALYZE); } catch (Throwable $e) { echo "Caught " . $e; }';
+        // TODO: Figure out why we need an error handler for this to work.
+        var analysisWrapper = document.getElementById('eval_wrapper_source').innerText;
         code = "?>" + code;
-        doRunWithWrapper(analysisWrapper, code, false);
+        doRunWithWrapper(analysisWrapper, code, false, 'PHP code ran without any output');
     });
     analyze_button.addEventListener('click', function () {
         output_area.innerText = '';
+        analyze_button.textContent = "Analyzing"
+        disableButtons();
         var code = editor.getValue();
         var query = new URLSearchParams();
         if (code.length < 1024) {
@@ -82,7 +116,7 @@ function init() {
             history.replaceState({}, document.title, "?" + query.toString());
         }
         var analysisWrapper = document.getElementById('phan_runner_source').innerText;
-        doRunWithWrapper(analysisWrapper, code, true);
+        doRunWithWrapper(analysisWrapper, code, true, 'Phan did not detect any errors');
     });
 }
 
@@ -96,6 +130,7 @@ var phpModuleOptions = {
         }
         if (didInit) {
             combinedOutput += text + "\n";
+            combinedHTMLOutput += text + "\n";
         }
     },
     printErr: function (text) {
@@ -105,6 +140,7 @@ var phpModuleOptions = {
             text = Array.prototype.slice.call(arguments).join(' ');
         }
         if (didInit) {
+            combinedHTMLOutput += '<span class="stderr">' + text + "</span>\n";
             combinedOutput += text + "\n";
         }
     }
