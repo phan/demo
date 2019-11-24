7f904cb953048298e23e4e340c073cfa4a4dc511
Tyson Andre <tandre@themeetgroup.com>
2019-11-24 11:47:48 -0500

    Support building/running with php 8.0.0-dev
    Add NO_COLOR=1 so that Phan won't emit ansi color codes in the HTML
    
    Update the build script

--- build_php8.sh
+++ build_php8.sh
@@ -3,10 +3,9 @@
 # TODO: https://emscripten.org/docs/porting/Debugging.html
 set -xeu
 
-PHP_VERSION=7.4.0RC6
-PHP_PATH=php-$PHP_VERSION
+PHP_PATH=php-src-master
 AST_PATH=ast-1.0.4
-PHAN_VERSION=2.4.4
+PHAN_VERSION=2.4.5-dev
 PHAN_PATH=phan-$PHAN_VERSION.phar
 
 if ! type emconfigure 2>/dev/null >/dev/null ; then
@@ -14,13 +13,10 @@ if ! type emconfigure 2>/dev/null >/dev/null ; then
     exit 1
 fi
 
-
 echo "Get PHP source"
 if [ ! -d $PHP_PATH ]; then
-    if [ ! -e $PHP_PATH.tar.xz ]; then
-        wget https://www.php.net/distributions/$PHP_PATH.tar.xz
-    fi
-    tar xf $PHP_PATH.tar.xz
+    echo "ERROR: Expected $PHP_PATH/ to exist"
+    exit 1
 fi
 
 echo "Apply error handler patch"
@@ -76,6 +72,24 @@ emconfigure ./configure \
   --disable-mbregex \
   --enable-tokenizer
 
+if false; then
+    # placeholder - re2c isn't working with emconfigure
+    for file in ext/json/json_scanner.c \
+            ext/json/php_json_scanner_defs.h \
+            ext/pdo/pdo_sql_parser.c \
+            ext/phar/phar_path_check.c \
+            ext/standard/url_scanner_ex.c \
+            ext/standard/var_unserializer.c \
+            sapi/phpdbg/phpdbg_lexer.c \
+            Zend/zend_ini_scanner.c \
+            Zend/zend_ini_scanner_defs.h \
+            Zend/zend_language_scanner.c \
+            Zend/zend_language_scanner_defs.h; do
+        cp path/to/php-src/$file $file
+    done
+fi
+
+
 echo "Build"
 # -j5 seems to work for parallel builds
 emmake make clean
@@ -95,8 +109,8 @@ emcc $CFLAGS \
   -s INVOKE_RUN=0 \
   -s ERROR_ON_UNDEFINED_SYMBOLS=0 \
   --preload-file $PHAN_PATH \
-  libs/libphp7.a pib_eval.o -o out/php.html
+  libs/libphp.a pib_eval.o -o out/php.html
 
-cp out/php.wasm out/php.js out/php.data ..
+cp out/php.wasm out/php.js out/php.data ../8
 
 echo "Done"
